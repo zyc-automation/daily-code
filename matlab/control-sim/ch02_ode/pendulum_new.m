ac48080ca95b9ffe44b72e67a73fef688a63aaf5
zyc-automation <1696516274@qq.com>
2026-09-09 23:42:23 +0800

    2026-09-09 daily check-in

--- matlab/control-sim/ch02_ode/pendulum_new.m
+++ matlab/control-sim/ch02_ode/pendulum_new.m
@@ -3,8 +3,8 @@ g=9.8;
 L=1;
 
 
-pendulum_ode=@(t,z) [z(2); -(g/L)*z(1)];
-theta0=deg2rad(90);
+pendulum_ode=@(t,z) [z(2); -(g/L)*sin(z(1))];
+theta0=deg2rad(30);
 z0=[theta0; 0];
 tspan=[0,10];
 [t,z]=ode45(pendulum_ode,tspan,z0);
